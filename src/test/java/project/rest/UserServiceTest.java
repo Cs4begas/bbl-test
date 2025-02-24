c4af7572f29c36178152b6afc2483d9d99d233b3
Nattapan Meepiean <formulae445@gmail.com>
2025-02-24 19:30:49 +0700

    feat(user): add unit-test for delete and update

--- src/test/java/project/rest/UserServiceTest.java
+++ src/test/java/project/rest/UserServiceTest.java
@@ -35,7 +35,7 @@ public class UserServiceTest {
 
     @Test
     public void whenGetUserById_ThenReturnNotFoundException(){
-        assertThrows(NotFoundException.class, () -> userService.getUserById(2));
+        assertThrows(NotFoundException.class, () -> userService.getUserById(555));
     }
 
     @Test
@@ -75,22 +75,18 @@ public class UserServiceTest {
     @Test
     public void whenUpdateUser_ThenSuccessUpdateUser() {
         UserDTO userDTO = new UserDTO();
-        userDTO.setId(111);
-        userDTO.setName("User 3");
-        userDTO.setWebsite("www.user3.com");
-        userDTO.setPhone("123456789");
-
-        CompanyDTO companyDTO = new CompanyDTO();
-        companyDTO.setName("Company 1");
-        companyDTO.setCatchPhrase("www.company1.com");
-        companyDTO.setBs("987654321");
-        userDTO.setCompany(companyDTO);
+        userDTO.setId(2);
+        userDTO.setName("Test");
+        userDTO.setUsername("Antonette");
+        userDTO.setEmail("Shanna@melissa.tv");
+        userDTO.setPhone("010-692-6593");
+        userDTO.setWebsite("anastasia.net");
 
         AddressDTO addressDTO = new AddressDTO();
-        addressDTO.setStreet("Street 1");
-        addressDTO.setSuite("Suite 1");
-        addressDTO.setCity("City 1");
-        addressDTO.setZipcode("12345");
+        addressDTO.setStreet("Victor Plains");
+        addressDTO.setSuite("Suite 879");
+        addressDTO.setCity("Wisokyburgh");
+        addressDTO.setZipcode("90566-7771");
 
         GeoDTO geoDTO = new GeoDTO();
         geoDTO.setLat("-37.3159");
@@ -98,11 +94,24 @@ public class UserServiceTest {
 
         addressDTO.setGeo(geoDTO);
 
+        CompanyDTO companyDTO = new CompanyDTO();
+        companyDTO.setName("Deckow-Crist");
+        companyDTO.setCatchPhrase("Proactive didactic contingency");
+        companyDTO.setBs("synergize scalable supply-chains");
 
-        UserDTO createdUser = userService.updateUser(userDTO.getId(), userDTO);
 
-        assertNotNull(createdUser);
-        assertEquals(111, createdUser.getId());
+        UserDTO updateUser = userService.updateUser(2, userDTO);
+
+        assertNotNull(updateUser);
+        assertEquals(2, updateUser.getId());
+        assertEquals("Test", updateUser.getName());
+        assertNotEquals("Leanne Graham", updateUser.getName());
+    }
+
+    @Test
+    public void whenDeleteUser_ThenSuccessDeleteUser() {
+        userService.deleteUser(1);
+        assertEquals(9, userService.getUsers().size());
     }
 
 
